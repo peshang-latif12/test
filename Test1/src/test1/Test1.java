13319264c46c48ed8aaf0996ac0179994c7ce66d
peshang <peshang.313185154@uhd.edu.iq>
2019-11-18 15:31:22 +0300

    First commit

---
+++ Test1/src/test1/Test1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author ZETTA
+ */
+public class Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a,b,c;
+        a=1;
+        b=3;
+        c=a+b;
+        System.out.println(c);
+    }
+    
+}
